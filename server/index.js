5421ba02aef7c975de3d7602faa24f0351738ed7
AksLIntellipat <118931093+akslintellipat@users.noreply.github.com>
2024-04-14 11:09:36 +0530

    auth routes

--- server/index.js
+++ server/index.js
@@ -7,6 +7,9 @@ import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import multer from 'multer';
+import mongoose from 'mongoose';
+import { register } from './controllers/auth.js';
+import authroutes from './routes/auth.js';
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -34,3 +37,22 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
+
+// ROUTES WITH FILES
+app.post('auth/register', upload.single('picture'), register);
+
+// ROUTES
+app.use('/auth', authroutes);
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server PORT: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`${err} \n Could not connect`);
+  });
